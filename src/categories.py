ce8744ea02145c28eb917e5d897621a2f65ef9c7
KelsieMcDonald-2003 <kelsie.lin.mcdonald@gmail.com>
2024-07-25 21:26:37 -0700

    Added beta version categories.

---
+++ src/categories.py
@@ -0,0 +1,19 @@
+class Categories:
+    def category(self, ):
+        self.easyCategory = [
+            ["Animals"],
+            ["Movies"],
+            ["Book Genres"]
+        ]
+        
+        self.normalCategory = [
+            ["Actors"],
+            ["Book Titles"],
+            ["Fruits"]
+        ]
+        
+        self.hardCategory = [
+            ["Countries/States"],
+            ["Weather/Temperatures"],
+            ["Musical Instruments"]
+        ]
